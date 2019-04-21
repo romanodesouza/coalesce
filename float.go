408b4645519608a2caa8ca79a0f71b1ac543c3e0
Romano de Souza <romanoaugusto88@gmail.com>
2019-04-21 20:32:17 +0100

    Fix LICENSE

--- float.go
+++ float.go
@@ -1,3 +1,7 @@
+// Copyright 2018 Romano de Souza. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 package coalesce
 
 // Float32 returns the first non-nil value in a list of pointers to float32.
